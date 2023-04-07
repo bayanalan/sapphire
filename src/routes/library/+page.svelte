0443b56fb99ff218987200e9f845d93b731c3d81
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2023-04-06 19:18:50 -0700

    Removed Open Book Buttons

--- src/routes/library/+page.svelte
+++ src/routes/library/+page.svelte
@@ -23,17 +23,26 @@
 </div>
 
 <div class="flex flex-wrap gap-4 mx-5">
+  
+  <a href="/book/picklesandjerimiahpart1">
   <div> 
       <img src="/P+J+P1.png" width="225" class="mx-8 pt-1 mt-5" alt="Pickles and Jerimiah Part 1">
-      <a class="poppins btn btn-outline rounded-full btn-sm mt-5 mx-20 hover:shadow-xl" href="/book/picklesandjerimiahpart1">Open Book</a>
     </div>
+  </a>
   
+  <a href="/book/picklesandjerimiahpart2">
     <div>
       <img src="/P+J+P2.png" width="225" class="mx-8 pt-1 mt-5" alt="Pickles and Jerimiah Part 2">
-      <a class="poppins btn btn-outline rounded-full btn-sm mt-5 mx-20 hover:shadow-xl" href="/book/picklesandjerimiahpart2">Open Book</a>
     </div>
+  </a>
+    
+  <a href="/book/picklesandjerimiahpart3">
+  <div>
+      <img src="/P+J+P3.png" width="225" class="mx-8 pt-1 mt-5" alt="Pickles and Jerimiah Part 3">
+    </div>
+  </a>
   
-  </div>
+</div>
 
 
 <div class="flex flex-wrap gap-4 mx-5">
@@ -49,12 +58,13 @@
       </div>
 
       <div class="flex flex-wrap gap-4 mx-5">
+        
+        <a href="/book/abaloneinheaven">
         <div> 
             <img src="/abalone1.png" width="225" class="mx-8 pt-1 mt-5" alt="Abalone in Heaven">
-            <a class="poppins btn btn-outline rounded-full btn-sm mt-5 mx-20 hover:shadow-xl" href="/book/abaloneinheaven">Open Book</a>
           </div>
+          <a>
         </div>
-  
 <div class="flex flex-wrap gap-4 mx-5">
   
   <div class="pt-20 pl-6">
@@ -69,11 +79,11 @@
       
       <div class="flex flex-wrap gap-4 mx-5">
       
-      <div> 
+        <a href="/book/buzzthebee">
+        <div> 
         <img src="/buzzthebee.png" width="225" class="mx-8 pt-1 mt-5" alt="Buzz the Bee">
-        <a class="poppins btn btn-outline btn-sm rounded-full mt-5 mx-20 hover:shadow-xl" href="/book/buzzthebee">Open Book</a>
       </div>
-
+</a>
 
   </div>
 
@@ -99,10 +109,11 @@
           </div>
     
           <div class="flex flex-wrap gap-4 mx-5">
+            <a href="/book/findingyourvoice">
             <div> 
                 <img src="/findyourvoice.png" width="225" class="mx-8 pt-1 mt-5" alt="Finding Your Voice">
-                <a class="poppins btn btn-outline rounded-full btn-sm mt-5 mx-20 hover:shadow-xl" href="/book/findingyourvoice">Open Book</a>
               </div>
+            </a>
             </div>
     
     <center>
@@ -141,17 +152,18 @@
     
           </div>
           <div class="flex flex-wrap gap-4 mx-5">
+            <a href="/book/aliceinwonderland">
             <div> 
                 <img src="/aliceinwonderland.png" width="225" class="mx-8 pt-1 mt-5" alt="Alice in Wonderland">
-                <a class="poppins btn btn-outline rounded-full btn-sm mt-5 mx-20 hover:shadow-xl" href="/book/aliceinwonderland">Open Book</a>
               </div>
-            
+            </a>
 
             <div class="flex flex-wrap gap-4 mx-5">
+              <a href="/book/throushthelookingglass">
               <div> 
                   <img src="/glass.png" width="225" class="mx-8 pt-1 mt-5" alt="Through the Looking Glass">
-                  <a class="poppins btn btn-outline rounded-full btn-sm mt-9 mx-20 hover:shadow-xl" href="/book/throughthelookingglass">Open Book</a>
                 </div>
+              </a>
               </div>
         </div>
         
