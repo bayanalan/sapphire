a471993d19c58cb019bfc8244180e0af009fdf1a
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2023-05-03 19:50:21 -0700

    SUPER SECRET

---
+++ src/routes/setup/pricing/+page.svelte
@@ -0,0 +1,11 @@
+<img class="mx-auto pt-20" width="400" src="submo.png" alt="$4/month. What you get: NO ADS and all books." />
+
+<center> <a class="shadow-xl poppins rounded-full btn btn-outline mt-6 hover:shadow-xl" href="https://buy.stripe.com/test_4gw180fTs9r8cHCbIJ" target="blank">Subscribe</a> </center>
+
+<img class=" mx-auto pt-20" width="400" src="subye.png" alt="$40/year. What you get: NO ADS and all books." />
+
+<center> <a class="shadow-xl rounded-full poppins btn btn-outline mt-6 hover:shadow-xl" href="https://buy.stripe.com/test_bIY7wo36G8n45facMO" target="blank">Subscribe</a> </center>
+
+<img class="mx-auto pt-20" width="400" src="subone.png" alt="$150. What you get: NO ADS, one-time payment, and all books." />
+
+<center> <a class="shadow-xl poppins btn rounded-full btn-outline mt-6 hover:shadow-xl" href="https://buy.stripe.com/test_9AQaIA9v432K6je6os" target="blank">Subscribe</a> </center>
---
+++ src/routes/setup/signup/+page.svelte
@@ -0,0 +1,52 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign Up</title>
+  <!-- Load TailwindCSS -->
+  <link rel="stylesheet" href="https://unpkg.com/tailwindcss@0.25.0/dist/tailwind.min.css">
+  <!-- Load DaisyUI -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/daisyui@1.14.4/dist/daisyui.min.css" />
+</head>
+<body>
+  <!-- Center the sign-up form on the page -->
+  <div class="flex justify-center items-center h-screen">
+    <div class="w-full max-w-md">
+      <!-- Sign-up form using DaisyUI classes -->
+      <form class="bg-white shadow-lg rounded-2xl px-8 pt-6 pb-8 mb-4">
+        <h1 class="text-2xl font-bold text-center mb-4">Sign Up</h1>
+        <div class="mb-4">
+        </div>
+        <div class="mb-4">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a class="ml-2 poppins block poppins text-gray-700 text-sm font-bold mb-2">
+          Email
+          </a>
+          <input class="poppins appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter email">
+        </div>
+        <div class="mb-4">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a class="ml-2 block poppins text-gray-700 text-sm font-bold mb-2">
+           Password
+          </a>
+          <input class="poppins appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter password">
+        </div>
+        <div class="mb-4">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a class="ml-2 block poppins text-gray-700 text-sm font-bold mb-2">
+            Confirm Password
+          </a>
+          <input class="poppins appearance-none border rounded-full w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password-confirm" type="password" placeholder="Confirm password">
+        </div>
+        <div class="pb-4">
+          <a class="ml-2 link link-hover poppins" href="/signin">Already have an account? Sign In</a>
+        </div>
+        <div class="mb-4">
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a class="w-full btn btn-outline poppins py-2 px-4 rounded-full">Sign Up</a>
+      </div>
+      </form>
+    </div>
+  </div>
+</body>
+</html>
