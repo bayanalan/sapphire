86228cc0db6c5c0e5b14c78fd01f8d55e7095379
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2023-05-06 12:24:49 -0700

    Changed Navbar

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -13,7 +13,8 @@
 <!-- svelte-ignore a11y-missing-attribute -->
 <center class="mt-10"><a href="mailto:bayan.alan.jones@gmail.com" target="_blank" class="text-2xl poppins">Email: bayan.alan.jones@gmail.com</a></center>
 
-<center class="mt-10"><a href="/pricing" class="text-2xl poppins">Plan: $4/Month</a></center>
+<!-- svelte-ignore a11y-missing-attribute -->
+<center class="mt-10"><a class="text-2xl poppins">Plan: $4/Month</a></center>
 
 <center class="mt-10"><a href="/signin" class="rounded-full btn btn-outline text-white bg-red-500 text-2xl poppins">Sign Out</a></center>
   
