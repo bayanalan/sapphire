9e1a84016399be4017afeecc731a35b9e9de598b
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2023-05-10 10:46:26 -0700

    Added Game

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -1,5 +1,8 @@
-  <!-- <script>
+<script>
+  // Import necessary Svelte functions
   import { onMount, beforeUpdate } from 'svelte';
+
+  // Initialize game variables
   let playerX = 50;
   let playerY = 250;
   let playerVelocity = 0;
@@ -7,11 +10,13 @@
   let score = 0;
   let gameOver = false;
 
+  // Constants for the game
   const GRAVITY = 0.5;
   const JUMP_FORCE = 8;
   const PILLAR_SPEED = 2;
   const PILLAR_GAP = 200;
 
+  // Function to add a new pillar to the game
   const addPillar = (x) => {
     const minGap = 100;
     const maxGap = 300;
@@ -25,6 +30,7 @@
     });
   };
 
+  // Function to check if the player has collided with a pillar or boundary
   const checkCollision = () => {
     const playerLeft = playerX;
     const playerRight = playerX + 55;
@@ -33,7 +39,10 @@
 
     for (const pillar of pillars) {
       if (playerRight > pillar.x && playerLeft < pillar.x + 50) {
-        if (playerTop < pillar.topHeight || playerBottom > 300 - pillar.bottomHeight) {
+        if (
+          playerTop < pillar.topHeight ||
+          playerBottom > 300 - pillar.bottomHeight
+        ) {
           gameOver = true;
         } else if (!pillar.passed) {
           pillar.passed = true;
@@ -47,6 +56,7 @@
     }
   };
 
+  // Function to restart the game
   const restartGame = () => {
     score = 0;
     pillars = [];
@@ -57,6 +67,7 @@
     addPillar(500);
   };
 
+  // Function to handle player controls
   const handleKeyDown = (event) => {
     if (event.code === 'Space') {
       playerVelocity = -JUMP_FORCE;
@@ -67,15 +78,18 @@
     }
   };
 
+  // Setup game loop and event listener on mount
   onMount(() => {
     window.addEventListener('keydown', handleKeyDown);
     addPillar(500);
 
+    // Main game loop
     const gameLoop = () => {
       if (gameOver) {
         return;
       }
 
+      // Update pillars
       for (const pillar of pillars) {
         pillar.x -= PILLAR_SPEED;
         if (pillar.x < -50) {
@@ -83,67 +97,85 @@
         }
       }
 
-      if (pillars.length === 0 || pillars[pillars.length - 1].x < 500 - PILLAR_GAP) {
+      // Add new pillar if necessary
+      if (
+        pillars.length === 0 ||
+        pillars[pillars.length - 1].x < 500 - PILLAR_GAP
+      ) {
         addPillar(500);
       }
 
+      // Update player position
       playerVelocity += GRAVITY;
       playerY += playerVelocity;
 
-      checkCollision();
-    };
+// Check for collisions
+checkCollision();
+};
 
-    let lastTime = performance.now();
-    const gameTick = () => {
-      const now = performance.now();
-      const deltaTime = now - lastTime;
-      lastTime = now;
+// Define gameTickTimeout at the top-level scope
+let gameTickTimeout;
 
-      gameLoop();
+// ...
 
-      $: $gameTickTimeout = setTimeout(gameTick, Math.max(0, 16.67 - deltaTime));
-    };
+// Game tick function to maintain consistent frame rate
+let lastTime = performance.now();
+const gameTick = () => {
+  const now = performance.now();
+  const deltaTime = now - lastTime;
+  lastTime = now;
 
-    gameTick();
-  });
+  // Run the game loop
+  gameLoop();
 
-  beforeUpdate(() => {
-    if (gameOver) {
-      window.removeEventListener('keydown', handleKeyDown);
-    }
-  });
+  // Schedule the next game tick
+  gameTickTimeout = setTimeout(gameTick, Math.max(0, 16.67 - deltaTime));
+};
+
+// Start the game tick
+gameTick();
+});
+
+// Cleanup event listener before update
+beforeUpdate(() => {
+if (gameOver) {
+window.removeEventListener('keydown', handleKeyDown);
+}
+});
 </script>
 
 <div class="flex flex-col items-center space-y-4">
+  <!-- Game container -->
   <div class="relative w-80 h-80 border border-gray-600">
+    <!-- Player character -->
     <div
       class="absolute top-0 left-0 w-12 h-50 bg-cover"
-      style={{ backgroundImage: "url('/bookoptions.png')" }}
-      style={{ top: `${playerY}px`, left: `${playerX}px` }}
+      style={`background-image: url('/bookoptions.png'); top: ${playerY}px; left: ${playerX}px`}
     />
     {#each pillars as pillar}
+      <!-- Bottom pillar -->
       <div
         class="absolute bottom-0 left-0 w-50 h-[300px] bg-cover"
-        style={{ backgroundImage: "url('/picklesandjeremiah1.png')" }}
-        style={{ transform: `translateX(${pillar.x}px) translateY(${300 - pillar.bottomHeight}px)` }}
+        style={`background-image: url('/picklesandjeremiah1.png'); transform: translateX(${pillar.x}px) translateY(${300 - pillar.bottomHeight}px)`}
       />
+      <!-- Top pillar -->
       <div
         class="absolute top-0 left-0 w-50 h-[300px] bg-cover"
-        style={{ backgroundImage: "url('/picklesandjeremiah1.png')" }}
-        style={{ transform: `translateX(${pillar.x}px) translateY(-${pillar.topHeight}px)` }}
+        style={`background-image: url('/picklesandjeremiah1.png'); transform: translateX(${pillar.x}px) translateY(-${pillar.topHeight}px)`}
       />
     {/each}
   </div>
+  <!-- Game over and score display -->
+{#if gameOver}
+<div class="text-4xl font-bold text-red-500">Game Over!</div>
+<button
+class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+on:click={() => restartGame()}
+>
+Restart
+</button>
+{:else}
+<div class="text-4xl font-bold text-gray-500">Score: {score}</div>
+{/if}
 
-  {#if gameOver}
-    <div class="text-4xl font-bold text-red-500">Game Over!</div>
-    <button
-      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-      on:click={() => restartGame()}
-    >
-      Restart
-    </button>
-  {:else}
-    <div class="text-4xl font-bold text-gray-500">Score: {score}</div>
-  {/if}
-</div> -->
+</div>
