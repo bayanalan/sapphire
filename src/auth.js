faa84cd44c2444cb7e109e59fad81a9371ac7486
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2023-09-18 20:16:13 -0700

    aider: Uncommented code in `src/auth.js` to enable authentication with Supabase.
    # Aider chat conversation:
    
    USER: finish adding supabase auth to the sign up/sign in pages.When you try to go directly to the library page without signing up, it will redirect you to the homepage.
    If you have already signed up, you cannot go back to the sign up/ sign in pages until you sign out. you can also not go back to the homepage after signing up.
    
    ASSISTANT: To implement the requested changes, we need to modify the following files:
    
    1. `src/routes/signin/+page.svelte`
    2. `src/routes/setup/signup/+page.svelte`
    3. `src/routes/library/available/+page.svelte`
    4. `src/routes/library/purchased/+page.svelte`
    5. `src/auth.js`
    
    However, I can't propose changes to these files because they are currently read-only. Please add these files to the chat so I can suggest modifications.
    
    USER: I added these *read-write* files: src/routes/library/purchased/+page.svelte, src/routes/setup/signup/+page.svelte, src/routes/signin/+page.svelte, src/auth.js, src/routes/library/available/+page.svelte
    
    ASSISTANT: Great, now we can proceed with the changes.
    
    First, we need to uncomment the code in `src/auth.js` to enable authentication with Supabase.
    
    ```javascript
    src/auth.js
    <<<<<<< HEAD
    // import { createAuth } from 'svelte-auth';
    // import { supabaseScheme } from '@sveltejs/kit/auth';
    // import supabase from '$lib/supabase.js';
    
    // const auth = createAuth({
    //     strategies: {
    //       supabase: {
    //         scheme: supabaseScheme,
    //         options: {
    //           supabase,
    //         },
    //       },
    //     },
    //   });
    =======
    import { createAuth } from 'svelte-auth';
    import { supabaseScheme } from '@sveltejs/kit/auth';
    import supabase from '$lib/supabase.js';
    
    const auth = createAuth({
        strategies: {
          supabase: {
            scheme: supabaseScheme,
            options: {
              supabase,
            },
          },
        },
      });
    >>>>>>> updated
    ```
    
    Next, we need to modify the sign-in and sign-up pages to use the `auth` object from `src/auth.js` to handle authentication. We'll also add code to redirect users to the library page after successful sign-in or sign-up, and to the home page if they're not authenticated.
    
    I'll need more information about your application structure to provide the exact changes. Specifically, I need to know the path to the home page and the method to sign out. Could you provide these details?

--- src/auth.js
+++ src/auth.js
@@ -1,16 +1,15 @@
-// import { createAuth } from 'svelte-auth';
-// import { supabaseScheme } from '@sveltejs/kit/auth';
-// import supabase from '$lib/supabase.js';
+import { createAuth } from 'svelte-auth';
+import { supabaseScheme } from '@sveltejs/kit/auth';
+import supabase from '$lib/supabase.js';
 
-
-// const auth = createAuth({
-//     strategies: {
-//       supabase: {
-//         scheme: supabaseScheme,
-//         options: {
-//           supabase,
-//         },
-//       },
-//     },
-//   });
+const auth = createAuth({
+    strategies: {
+      supabase: {
+        scheme: supabaseScheme,
+        options: {
+          supabase,
+        },
+      },
+    },
+  });
   
