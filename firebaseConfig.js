3047e6cc163d6c9e5170d45d7723b8067957c4c3
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2023-12-09 20:11:20 -0700

    aider: Modified `firebaseConfig.js` to read environment variables from the `.env` file for the Firebase configuration.
    # Aider chat conversation:
    
    USER: make sure the other files use teh .env file for the keys.
    
    ASSISTANT: To ensure that the other files use the `.env` file for the keys, we need to modify the `firebaseConfig.js` file to read the environment variables instead of using hardcoded values. We will use the `import.meta.env` object provided by Vite to access environment variables.
    
    Here is the *SEARCH/REPLACE* block for the `firebaseConfig.js` file:
    
    firebaseConfig.js
    ```javascript
    <<<<<<< SEARCH
    // Your web app's Firebase configuration
    const firebaseConfig = {
      apiKey: "your-api-key", // Replace with your Firebase API key
      authDomain: "your-project-auth-domain",
      projectId: "your-project-id",
      storageBucket: "your-project-storage-bucket",
      messagingSenderId: "your-messaging-sender-id",
      appId: "your-app-id"
    };
    =======
    // Your web app's Firebase configuration
    const firebaseConfig = {
      apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
      authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
      projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
      storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
      messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
      appId: import.meta.env.VITE_FIREBASE_APP_ID
    };
    >>>>>>> REPLACE
    ```
    
    Make sure that you have the `.env` file at the root of your project with the actual values for these keys, and that Vite is configured to load environment variables from the `.env` file. The environment variables should be prefixed with `VITE_` to be exposed to your client-side code.
    
    Please note that if you have other files that also use these keys directly, you would need to update them similarly to use `import.meta.env` to access the environment variables. If you have such files, please provide them, and I will assist you with the necessary changes.

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-api-key", // Replace with your Firebase API key
-  authDomain: "your-project-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-project-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
